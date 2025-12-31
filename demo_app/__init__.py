30e565863ffc088d1231665c3795526583e13221
Dipanjal Maitra <dmaitra@paytrace.com>
2025-12-31 12:24:20 +0600

    lab-5: FIX template location

--- demo_app/__init__.py
+++ demo_app/__init__.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
 from poridhi_frame import PoridhiFrame
 from poridhi_frame.middlewares import ErrorHandlerMiddleware
 
-app = PoridhiFrame(template_dir="./demo_app/templates")
+cwd = Path(__file__).resolve().parent
+app = PoridhiFrame(template_dir=f"{cwd}/templates")
 exception_handler_middleware = ErrorHandlerMiddleware(
     app=app
 )
