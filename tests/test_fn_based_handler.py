a319b17673a27e9ef17e47cfb4bc96cd556be9c6
dipanjal <dipanjalmaitra@gmail.com>
2026-01-08 16:23:15 +0600

    lab-8: add test for method not allowed

--- tests/test_fn_based_handler.py
+++ tests/test_fn_based_handler.py
@@ -1,7 +1,10 @@
 import pytest
 from webob.response import Response
 
+from poridhi_frame.exceptions import MethodNotAllowed
+from poridhi_frame.middlewares import ErrorHandlerMiddleware
 from tests.constants import BASE_URL
+from tests.utils.test_framework import TestFrameworkBuilder
 
 
 def test_client_can_send_requests(app, client):
@@ -76,3 +79,29 @@ def test_explicitly_registered_route(app, client):
 
     response = client.get(f"{BASE_URL}/test")
     assert response.text == RESPONSE_TEXT
+
+
+def test_method_not_allowed_request():
+    app = TestFrameworkBuilder().build()
+    client = app.test_session()
+
+    @app.route("/home", allowed_methods=["post"])
+    def home(req):
+        return Response("Hello")
+
+    with pytest.raises(MethodNotAllowed):
+        client.get(f"{BASE_URL}/home")
+
+
+def test_method_not_allowed_request_handled():
+    app = TestFrameworkBuilder().build()
+    app.add_middleware(ErrorHandlerMiddleware)
+    client = app.test_session()
+
+    @app.route("/home", allowed_methods=["post"])
+    def home(req):
+        return Response("Hello")
+
+    response = client.get(f"{BASE_URL}/home")
+    response.status_code = 405
+
