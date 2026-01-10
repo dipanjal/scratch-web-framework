e18e801251968790eb7bd5c71d4888191354c09d
dipanjal <dipanjalmaitra@gmail.com>
2026-01-10 23:04:57 +0600

    lab-9: move to_dict in JSONUtils helper class

--- poridhi_frame/helpers.py
+++ poridhi_frame/helpers.py
@@ -1,3 +1,7 @@
+from collections.abc import Sequence
+from enum import Enum
+from typing import Any
+
 from parse import parse
 from webob.request import Request
 
@@ -56,6 +60,26 @@ class RoutingHelper:
         return route_def
 
 
+class JSONUtils:
+    @classmethod
+    def to_dict(cls, content: dict | list | Any):
+        if content is None:
+            return None
+        if isinstance(content, (str, int, float, bool)):
+            return content
+        if isinstance(content, Enum):
+            return content.value
+        if hasattr(content, "__dict__"):
+            return cls.to_dict(content.__dict__)
+        if isinstance(content, dict):
+            return {
+                key: cls.to_dict(value)
+                for key, value in content.items()
+            }
+        if isinstance(content, (list, tuple, set, Sequence)):
+            return [cls.to_dict(item) for item in content]
+
+        return content
 
 
 
