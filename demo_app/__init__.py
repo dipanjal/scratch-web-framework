84837aae2a8a258417f5a6b81c5cf602f7228e9f
dipanjal <dipanjalmaitra@gmail.com>
2026-01-21 03:05:15 +0600

    ORM-Lab-5: ORM integration with Demo App

--- demo_app/__init__.py
+++ demo_app/__init__.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 
 from demo_app.middlewares import TokenMiddleware
+from demo_app.models.book import Book
 from poridhiweb import PoridhiFrame
 from poridhiweb.middlewares import (
     ErrorHandlerMiddleware,
     ExecutionTimeMiddleware,
     ReqResLoggingMiddleware
 )
+from poridhiweb.orm.db_factory import DatabaseFactory, Dialect
+
 cwd = Path(__file__).resolve().parent
 app = PoridhiFrame(
     template_dir=f"{cwd}/templates",
@@ -16,3 +19,6 @@ app.add_middleware(TokenMiddleware)
 app.add_middleware(ErrorHandlerMiddleware)
 app.add_middleware(ExecutionTimeMiddleware)
 app.add_middleware(ReqResLoggingMiddleware)
+
+db = DatabaseFactory(dialect=Dialect.SQLITE).get_connection("./myapp.db")
+db.create(Book)
--- demo_app/models/book.py
+++ demo_app/models/book.py
@@ -1,7 +1,14 @@
-from typing import NamedTuple
+from poridhiweb.orm.column import PrimaryKey, Column
+from poridhiweb.orm.table import Table
 
 
-class Book(NamedTuple):
-    id: int
-    name: str
-    author: str
+class Author(Table):
+    id = PrimaryKey()
+    name = Column(str)
+    age = Column(int)
+
+
+class Book(Table):
+    id = PrimaryKey()
+    name = Column(str)
+    author = Column(str)
